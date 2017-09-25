e160cb1d7c6d62f76a3d8ef7a75d8f7d5826dd58
siva <sivakumarrus@gmail.com>
2017-09-24 19:36:14 -0700

    Deleteing

--- American Express - Dashboard_files/satellite-591ddf0464746d08a6002446.js
+++
@@ -1,8 +0,0 @@
-//console.log("dcr page analytics fired ... ");
-/* Business unit specific logic */
-if(_satellite.getVar("Business Unit")=="amexpressser")   
-  _satellite.track("myca code"); 
-else if(_satellite.getVar("Business Unit")=="amexpressmrent")
-  _satellite.track("mr code");
-else
-	_satellite.track("default page code");
